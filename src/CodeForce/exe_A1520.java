aa751649fbcb2a437a232b52abea09cb17851cf7
Bris <dangngocquan104@gmail.com>
2022-02-02 23:16:23 +0700

    Do some exe

---
+++ src/CodeForce/exe_A1520.java
@@ -0,0 +1,34 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A1520 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		String[] res = new String[t];
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+			int[] arr = new int[26];
+			String s = scanner.next();
+			arr[s.charAt(0) - 'A'] += 1;
+			res[i] = "YES";
+			for (int j = 1; j < n; j++ ) {
+				if (s.charAt(j) != s.charAt(j-1)) {
+					if (arr[s.charAt(j) - 'A'] > 0) {
+						res[i] = "NO";
+						break;
+					}else {
+						arr[s.charAt(j) - 'A']++;
+					}
+				}
+			}
+		}
+		scanner.close();
+		//Output
+		for (String i : res) {
+			System.out.println(i);
+		}
+	}
+}
