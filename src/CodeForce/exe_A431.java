42b97ec6edda5c1677f67dc49a7f7624441442b8
Bris <dangngocquan104@gmail.com>
2022-02-03 21:36:28 +0700

    +7 exe

---
+++ src/CodeForce/exe_A431.java
@@ -0,0 +1,19 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A431 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int[] a = new int[5];
+		for (int i = 1; i <=4; i++) a[i] = scanner.nextInt();
+		String s = scanner.next();
+		scanner.close();
+		//Solve
+		int res = 0;
+		for (int i = 0; i < s.length(); i++) res += a[s.charAt(i)-'0'];
+		//Output
+		System.out.println(res);
+	}
+}
---
+++ src/CodeForce/exe_B520.java
@@ -0,0 +1,31 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_B520 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		scanner.close();
+		//Solve
+		int res = 0;
+		if (n> m) {
+			res = n-m;
+		}else if (n < m) {
+			while (m> n) {
+				if (m %2 == 1) {
+					m += 1;
+					res++;
+				}else {
+					m /= 2;
+					res++;
+				}
+			}
+			res += n-m;
+		}
+		//Output
+		System.out.println(res);
+	}
+}
