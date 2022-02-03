f622c6421824d0fe4e2a07ff7bb380061d7d3bfa
Bris <dangngocquan104@gmail.com>
2022-02-03 15:15:15 +0700

    Do some exe

---
+++ src/CodeForce/exe_A151.java
@@ -0,0 +1,23 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A151 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int k = scanner.nextInt();
+		int l = scanner.nextInt();
+		int c = scanner.nextInt();
+		int d = scanner.nextInt();
+		int p = scanner.nextInt();
+		int nl = scanner.nextInt();
+		int np = scanner.nextInt();
+		scanner.close();
+		//Solve
+		int res = (Math.min(Math.min(k*l/nl, c*d), p/np))/n;
+		//Output
+		System.out.println(res);
+	}
+}
---
+++ src/CodeForce/exe_A460.java
@@ -0,0 +1,22 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A460 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		scanner.close();
+		//Solve
+		int count = 0;
+		while (n>0) {
+			n -=1;
+			count++;
+			if (count%m==0) n++;
+		}
+		//Output
+		System.out.println(count);
+	}
+}
