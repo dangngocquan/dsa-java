e9fc0838af64614c3a262bf9e28e62b1abea3467
Bris <dangngocquan104@gmail.com>
2022-02-03 23:36:56 +0700

    +8 exe

---
+++ src/CodeForce/exe_A630.java
@@ -0,0 +1,15 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A630 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+//		long n = scanner.nextLong();
+		scanner.close();
+		//Output
+		System.out.println(25);
+		
+	}
+}
---
+++ src/CodeForce/exe_B1475.java
@@ -0,0 +1,29 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_B1475 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		String[] res = new String[t];
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+			// n = a.2020 + b.2021 
+			res[i] = "NO";
+			for (int j = 0; j <= n/2021; j++) {
+				int k = n - 2021*j;
+				if (k%2020==0) {
+					res[i] = "YES";
+					break;
+				}
+			}
+		}
+		scanner.close();
+		//Output
+		for (String i: res) {
+			System.out.println(i);
+		}
+	}
+}
