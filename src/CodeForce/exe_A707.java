41d7bb47161c93a9305313bd517a8b804a131686
Bris <dangngocquan104@gmail.com>
2022-02-04 12:38:21 +0700

    +1 exe - branch afternoon_exam

---
+++ src/CodeForce/exe_A707.java
@@ -0,0 +1,20 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A707 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		String res = "#Black&White";
+		for (int i = 0; i < n*m; i++) {
+			String x = scanner.next();
+			if (x.equals("C") || x.equals("M") || x.equals("Y")) res = "#Color";
+		}
+		scanner.close();
+		//Output
+		System.out.println(res);
+	}
+}
