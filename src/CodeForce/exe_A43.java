2329ab01262c75f954409a1ffb2dd5f616256c58
Bris <dangngocquan104@gmail.com>
2022-02-04 22:59:15 +0700

    +3 exe - branch night_exam

---
+++ src/CodeForce/exe_A43.java
@@ -0,0 +1,31 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A43 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		String A = scanner.next();
+		int countA = 1;
+		String B = "";
+		int countB = 0;
+		for (int i = 1; i <= n-1; i++) {
+			String x = scanner.next();
+			if (x.equals(A)) {
+				countA++;
+			}else {
+				B = x;
+				countB++;
+			}
+		}
+		scanner.close();
+		//Output
+		if (countA > countB) {
+			System.out.println(A);
+		}else {
+			System.out.println(B);
+		}
+	}
+}
