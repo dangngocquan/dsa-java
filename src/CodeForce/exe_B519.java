9b0feb8cf4314bb4b27036a79a2451f77067ea90
Bris <dangngocquan104@gmail.com>
2022-02-05 23:24:29 +0700

    +8 exe - branch night_exam

---
+++ src/CodeForce/exe_B519.java
@@ -0,0 +1,21 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_B519 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		long sum1 = 0;
+		long sum2 = 0;
+		long sum3 = 0;
+		for (int i = 1; i <= n; i++) sum1 += scanner.nextLong();
+		for (int i = 1; i <= n-1; i++) sum2 += scanner.nextLong();
+		for (int i = 1; i <= n-2; i++) sum3 += scanner.nextLong();
+		scanner.close();
+		//output
+		System.out.println(sum1-sum2);
+		System.out.println(sum2 - sum3);
+	}
+}
