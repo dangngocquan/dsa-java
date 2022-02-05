0c91d2e8c3f9f06ae991dfde469d54c8a13703aa
Bris <dangngocquan104@gmail.com>
2022-02-05 20:26:06 +0700

    +3 exe - branch night_exam

---
+++ src/CodeForce/exe_B1335.java
@@ -0,0 +1,24 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_B1335 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		String[] res = new String[t];
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+//			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			res[i] = "";
+			for (int j = 0; j < n; j++) {
+				res[i] += (char)('a' + j%b);
+			}
+		}
+		scanner.close();
+		//Output
+		for (String i: res) System.out.println(i);
+	}
+}
