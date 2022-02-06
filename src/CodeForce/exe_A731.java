e0de5163e802ee4ea8c5e88b21717b8ad92051ff
Bris <dangngocquan104@gmail.com>
2022-02-06 16:03:13 +0700

    +5 exe - branch afternoon_exam

---
+++ src/CodeForce/exe_A731.java
@@ -0,0 +1,26 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A731 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.next();
+		scanner.close();
+		//Solve
+		char temp = 'a';
+		int res = 0;
+		for (int i = 0; i< s.length();i++) {
+			int x = Math.abs(s.charAt(i) - temp);
+			if (x > 13) {
+				res += 26-x;
+			}else {
+				res += x;
+			}
+			temp = s.charAt(i);
+		}
+		//Output
+		System.out.println(res);
+	}
+}
