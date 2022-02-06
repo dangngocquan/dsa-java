d34da37be0e36a06da3f326d91213ce262b7e14f
Bris <dangngocquan104@gmail.com>
2022-02-06 20:20:17 +0700

    +6 exe - branch night_exam

---
+++ src/CodeForce/exe_A577.java
@@ -0,0 +1,21 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A577 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int x = scanner.nextInt();
+		scanner.close();
+		//Solve
+		int res = 0;
+		//x/i <= n .... x <= n*i .... i >= x/n
+		for (int i = (int) Math.ceil(x*1.0/n) ; i <= n; i++) {
+			if (x%i==0) res++;
+		}
+		//Output
+		System.out.println(res);
+	}
+}
