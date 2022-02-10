199a0d834b8222414b690fa097f11cfc1031249f
Bris <dangngocquan104@gmail.com>
2022-02-10 16:24:48 +0700

    +6 exe - branch afternoon_exam

---
+++ src/CodeForce/exe_A1490.java
@@ -0,0 +1,28 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A1490 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		int[] ans = new int[t];
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+			int[] a = new int[n];
+			for (int j = 0; j < n; j++) a[j] = scanner.nextInt();
+			for (int j = 0; j < n-1; j++) {
+				int max = Math.max(a[j], a[j+1]);
+				int min = Math.min(a[j], a[j+1]);
+				while (max*1.0/min > 2) {
+					max = (max+1)/2;
+					ans[i] += 1;
+				}
+			}
+		}
+		scanner.close();
+		//Output
+		for (int i : ans) System.out.println(i);
+	}
+}
