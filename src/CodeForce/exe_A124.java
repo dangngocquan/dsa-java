d98874785e3149e4c2d81840b6882d6df5cc3e0e
Bris <dangngocquan104@gmail.com>
2022-02-10 23:15:24 +0700

    +4 exe - branch night_exam

---
+++ src/CodeForce/exe_A124.java
@@ -0,0 +1,20 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A124 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		scanner.close();
+		//Solve
+		int ans = n- Math.max(a, n-b-1);
+		// >= a+1
+		// >= n-b
+		//Output
+		System.out.println(ans);
+	}
+}
