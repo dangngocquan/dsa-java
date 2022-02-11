bd44d79c3b4c1e5c0b882dd071dffc862bbfbd93
Bris <dangngocquan104@gmail.com>
2022-02-11 17:21:36 +0700

    +2 exe - branch afternoon_exam

---
+++ src/CodeForce/exe_A1341.java
@@ -0,0 +1,11 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A1341 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		
+	}
+}
