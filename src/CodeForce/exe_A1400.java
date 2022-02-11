d8d807aae4761791bbfc9610932f8eb90f34e00c
Bris <dangngocquan104@gmail.com>
2022-02-11 14:39:28 +0700

    +4 exe - branch afternoon_exam

---
+++ src/CodeForce/exe_A1400.java
@@ -0,0 +1,21 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A1400 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		String[] ans = new String[t];
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+			String s = scanner.next();
+			ans[i] = "";
+			for (int j = 0; j < n; j++) ans[i] += s.charAt(n-1);
+		}
+		scanner.close();
+		//Output
+		for (String i : ans) System.out.println(i);
+	}
+}
---
+++ src/CodeForce/exe_B977.java
@@ -0,0 +1,35 @@
+package CodeForce;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class exe_B977 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		String s = scanner.next();
+		scanner.close();
+		//Solve
+		String ans = "";
+		int max = 0;
+		Map<String, Integer> m = new LinkedHashMap<String,Integer>();
+		for (int j = 0; j < n-1; j++) {
+			String x = s.substring(j,j+2);
+			if (m.containsKey(x)) {
+				m.replace(x, m.get(x), m.get(x)+1);
+			}else {
+				m.put(x, 1);
+			}
+		}
+		for (String x : m.keySet()) {
+			if (m.get(x) > max) {
+				max = m.get(x);
+				ans = x;
+			}
+		}
+		//Output
+		System.out.println(ans);
+	}
+}
