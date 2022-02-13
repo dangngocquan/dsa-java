39762afa2e7abc7eeffef0cab6a1047a17502dd8
Bris <dangngocquan104@gmail.com>
2022-02-13 17:17:15 +0700

    +4 exe - branch afternoon_exam

---
+++ src/CodeForce/exe_B1296.java
@@ -0,0 +1,23 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_B1296 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		int[] ans = new int[t];
+		for (int i = 0; i < t; i++) {
+			int s = scanner.nextInt();
+			ans[i] += s;
+			while (s >=10) {
+				ans[i] += s/10;
+				s = s/10 + s%10;
+			}
+		}
+		scanner.close();
+		//Output
+		for (int i : ans) System.out.println(i);
+	}
+}
--- src/CodeForce/exe_D1520.java
+++ src/CodeForce/exe_D1520.java
@@ -1,5 +1,7 @@
 package CodeForce;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class exe_D1520 {
@@ -7,24 +9,26 @@ public class exe_D1520 {
 		//Input
 		Scanner scanner = new Scanner(System.in);
 		int t = scanner.nextInt();
-		int[] ans = new int[t];
+		long[] ans = new long[t];
 		for (int i = 0; i < t; i++) {
-			int n = scanner.nextInt();
-			int[] a = new int[n+1];
-			
-			for (int j = 2; j <= n; j++) {
-				a[j] = scanner.nextInt();
-				for (int k = j-1; k >= 1; k--) {
-					if (a[j] - a[k] ==  j - k) {
-						b[j] = b[k] + 1;
-						ans[i] += b[j];
-						break;
-					}
+			int n = scanner.nextInt();		
+			// a[j] - a[i] == j - i <=> a[j] - j == a[i]-i
+			Map<Long, Long> m = new LinkedHashMap<Long,Long>();
+			for (long j = 1; j <= n; j++) {
+				long x = scanner.nextLong();
+				if (m.containsKey(x-j)) {
+					m.replace(x-j, m.get(x-j), m.get(x-j) + 1);
+				}else {
+					m.put(x-j, (long)1);
 				}
+			}
+			for (long j : m.keySet()) {
+				long x = m.get(j);
+				ans[i] += x*(x-1)/2;
 			}
 		}
 		scanner.close();
 		//Output
-		for (int i : ans) System.out.println(i);
+		for (long i : ans) System.out.println(i);
 	}
 }
