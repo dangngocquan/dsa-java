2a46f4c1119dc5b87206c180c66807c973056250
Bris <dangngocquan104@gmail.com>
2022-02-14 15:17:17 +0700

    +4 exe - branch afternoon_exam

---
+++ src/CodeForce/exe_A822.java
@@ -0,0 +1,19 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A822 {
+	static int recursive(int k) {
+		if (k==1) return 1;
+		return k*recursive(k-1);
+	}
+	
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int n = Math.min(scanner.nextInt(), scanner.nextInt());
+		scanner.close();
+		//Output
+		System.out.println(recursive(n));
+	}
+}
