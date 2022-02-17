e792e2eaefc66ff15f6af3e79ad9c1d6e4d75483
Bris <dangngocquan104@gmail.com>
2022-02-17 20:10:40 +0700

    +3 exe - branch master

---
+++ src/CodeForce/exe_A1366.java
@@ -0,0 +1,29 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A1366 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		long[] ans = new long[t];
+		for (int i = 0; i < t; i++) {
+			long a = scanner.nextLong();
+			long b = scanner.nextLong();
+			long x = Math.min(a, b);
+			long y = Math.max(a, b);
+			if (y >= 2*x) {
+				ans[i] = x;
+			}else {
+				ans[i] = y-x;
+				y = y - 2*ans[i];
+				x = x - ans[i];
+				ans[i] += (x+y)/3;
+			}
+		}
+		scanner.close();
+		//Output
+		for (long i: ans) System.out.println(i);
+	}
+}
