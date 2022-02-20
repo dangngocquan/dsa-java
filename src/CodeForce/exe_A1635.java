3a53cfe73693c298ed27eb923ebc870eb99246db
Bris <dangngocquan104@gmail.com>
2022-02-20 23:24:03 +0700

    +2 exe contest - branch master

---
+++ src/CodeForce/exe_A1635.java
@@ -0,0 +1,20 @@
+package CodeForce;
+
+import java.util.Scanner;
+
+public class exe_A1635 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		long[] ans = new long[t];
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+			ans[i] = scanner.nextLong();
+			for (int j = 1; j < n; j++) ans[i] = ans[i]|scanner.nextLong();
+		}
+		scanner.close();
+		//Output
+		for (long i : ans) System.out.println(i);
+	}
+}
