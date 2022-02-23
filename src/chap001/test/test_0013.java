86333b602c5392dc2c6daba1d81ea0c172a9e9c0
Bris <dangngocquan104@gmail.com>
2022-02-23 15:05:07 +0700

    +3 exe - branch master

--- src/chap001/test/test_0013.java
+++ src/chap001/test/test_0013.java
@@ -1,8 +1,7 @@
 package chap001.test;
 
 public class test_0013 {
-	public static void main(String[] args) {
-		String s = "anvsdg";
-		int a = s.length();
-	}
+	    public static void main(String[] args) {
+	    	
+	    }
 }
