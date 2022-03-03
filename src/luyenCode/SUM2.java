f2ced92f5b9bc36ae229c3a57365ce74b6a8956b
Bris <dangngocquan104@gmail.com>
2022-03-03 18:48:25 +0700

    +4 exe in luyenCode - branch master

---
+++ src/luyenCode/SUM2.java
@@ -0,0 +1,16 @@
+package luyenCode;
+
+import java.util.Scanner;
+
+public class SUM2 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		long n = scanner.nextLong();
+		scanner.close();
+		//Solve
+		long sum = n*(n+1)*(n+2)/3 - n*(n+1)/2;
+		//Output
+		System.out.println(sum);
+	}
+}
