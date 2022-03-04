be7d3b9ea8dc633d036b64d758f6d54077177af2
Bris <dangngocquan104@gmail.com>
2022-03-04 22:00:49 +0700

    +6 exe in codeForce - branch master

---
+++ src/codeForce/A1350.java
@@ -0,0 +1,27 @@
+package codeForce;
+
+import java.util.Scanner;
+
+public class A1350 {
+	static long getFixNumber(long n) {
+		for (long i = 2; i <= Math.sqrt(n); i++) {
+			if (n%i == 0) return n+i;
+		}
+		return n + n;
+	}
+	
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		long[] ans = new long[t];
+		for (int i = 0; i < t; i++) {
+			long n = scanner.nextLong();
+			long k = scanner.nextLong();
+			ans[i] = getFixNumber(n) + 2*(k-1);
+		}
+		scanner.close();
+		//Output
+		for (long i : ans) System.out.println(i);
+	}
+}
---
+++ src/codeForce/A1487.java
@@ -0,0 +1,30 @@
+package codeForce;
+
+import java.util.Scanner;
+
+public class A1487 {
+	public static void main(String[] args) {
+		//Input
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		int[] ans = new int[t];
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+			int min = Integer.MAX_VALUE;
+			int countMin = 1;
+			for (int j = 0; j < n; j++) {
+				int x = scanner.nextInt();
+				if (x < min) {
+					min = x;
+					countMin = 1;
+				}else if (x == min){
+					countMin++;
+				}
+			}
+			ans[i] = n - countMin;
+		}
+		scanner.close();
+		//Output
+		for (int i : ans) System.out.println(i);
+	}
+}
