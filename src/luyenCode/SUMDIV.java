7904695c5fb89cfed8ecf8cf6c2de68e5a35be1f
Bris <dangngocquan104@gmail.com>
2022-03-09 00:26:48 +0700

    +2 exe in luyenCode - branch master

---
+++ src/luyenCode/SUMDIV.java
@@ -0,0 +1,23 @@
+package luyenCode;
+
+import java.util.Scanner;
+
+public class SUMDIV {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		while (t-->0) {
+			long n = scanner.nextLong();
+			long sum = 0;
+			for (long i = 1; i <= Math.sqrt(n); i++) {
+				if (n % i== 0) {
+					long x = n/i;
+					sum += i;
+					if (x != i) sum += x;
+				}
+			}
+			System.out.println(sum);
+		}
+		scanner.close();
+	}
+}
