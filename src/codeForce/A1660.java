da998f464a4559f0d83756ce791d27f346580e3d
Bris <dangngocquan104@gmail.com>
2022-03-31 23:23:05 +0700

    +2 exexin codeforce cotest - branch master

---
+++ src/codeForce/A1660.java
@@ -0,0 +1,32 @@
+package codeForce;
+
+import java.util.Scanner;
+
+/**
+ * <a href = "https://codeforces.com/contest/1660/problem/0"> Link </a>
+ * @author Bris
+ * @version 1.0
+ * @since 9:36:06 PM -  Mar 31, 2022
+ */
+public class A1660 {
+	/**
+	 * The main method.
+	 * @param args Unused.
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		while  (t-->0) {
+			long a = scanner.nextLong();
+			long b = scanner.nextLong();
+			long ans;
+			if (a == 0) {
+				ans = 1;
+			}else {
+				ans = 2*b + a + 1;
+			}
+			System.out.println(ans);
+		}
+		scanner.close();
+	}
+}
