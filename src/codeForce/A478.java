26fc1c272cb816c86e79c9b9632b5b108ffc758a
Bris <dangngocquan104@gmail.com>
2022-04-09 22:17:52 +0700

    +1 exe in codeforce - brnach master

---
+++ src/codeForce/A478.java
@@ -0,0 +1,29 @@
+package codeForce;
+
+import java.util.Scanner;
+
+/**
+ * <a href = "https://codeforces.com/problemset/problem/478/A"> Link </a>.
+ * @author Bris
+ * @version 1.0
+ * @since 10:30:20 PM -  Apr 8, 2022
+ */
+public class A478 {
+	/**
+	 * The main method.
+	 * @param args Unused.
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			sum += scanner.nextInt();
+		}
+		if (sum % 5 == 0 && sum > 0) {
+			System.out.println(sum / 5);
+		}else {
+			System.out.println(-1);
+		}
+		scanner.close();
+	}
+}
