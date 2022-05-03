8bdcbb3379c8711e276c34eb33ce9369dac3b9b7
Bris <dangngocquan104@gmail.com>
2022-05-03 13:57:48 +0700

    +1 exe in codeforce - branch master

---
+++ src/codeForce/A1354.java
@@ -0,0 +1,39 @@
+package codeForce;
+
+import java.util.Scanner;
+
+/**
+ * <a href = "https://codeforces.com/problemset/problem/1354/A"> LInk </a>
+ * @author Bris
+ * @version 1.0
+ * @since 1:38:17 PM -  May 3, 2022
+ */
+public class A1354 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		while (t-->0) {
+			long a = scanner.nextLong();
+			long b = scanner.nextLong();
+			long c = scanner.nextLong();
+			long d = scanner.nextLong();
+			
+			if (b >= a) {
+				System.out.println(b);
+				continue;
+			}
+			
+			long totalTime = b;
+			
+			if (c <= d) {
+				System.out.println(-1);
+				continue;
+			}
+			
+			totalTime += (long)Math.ceil((a - b) * 1.0 / (c-d)) * c;
+			
+			System.out.println(totalTime);
+		}
+		scanner.close();
+	}
+}
