dcfa9a95444a76cdb0f73fdaec4f06b11467b2cc
Bris <dangngocquan104@gmail.com>
2022-05-04 22:53:56 +0700

    Test something - branch master

---
+++ src/other/test/Test022.java
@@ -0,0 +1,10 @@
+package other.test;
+
+import java.util.Scanner;
+
+public class Test022 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(123);
+    }
+}
---
+++ src/other/test/Test023.java
@@ -0,0 +1,10 @@
+package other.test;
+
+import java.util.Scanner;
+
+public class Test023 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(123);
+    }
+}
