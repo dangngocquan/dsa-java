15b5ba78a1c3f776b15247daa4e8b7c8c2709920
Bris <dangngocquan104@gmail.com>
2022-05-12 14:48:07 +0700

    +2 exe in codeforce - branch master

---
+++ src/codeForce/A1676.java
@@ -0,0 +1,28 @@
+package codeForce;
+
+import java.util.Scanner;
+
+/**
+ * <a href = "https://codeforces.com/contest/1676/problem/A"> Link </a>
+ * @author Bris
+ * @version 1.0
+ * @since 2:33:19 PM -  May 12, 2022
+ */
+public class A1676 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		while (t-- > 0) {
+			String s = scanner.next();
+			int sum = 0;
+			for (int i = 0; i < 3; i++) {
+				sum += Integer.parseInt(s.charAt(i) + "");
+			}
+			for (int i = 3; i < 6; i++) {
+				sum -= Integer.parseInt(s.charAt(i) + "");
+			}
+			System.out.println((sum == 0)? "YES" : "NO");
+		}
+		scanner.close();
+	}
+}
