1f09f4d614e761e35da4363db4ebb655ca070679
Bris <dangngocquan104@gmail.com>
2022-05-31 11:13:37 +0700

    +2 exe in codeforce - branch master

--- src/codeForce/A698.java
+++ src/codeForce/A698.java
@@ -17,52 +17,20 @@ public class A698 {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
 		int[] a = new int[n+2];
-		int[] b = new int[n+2];
+		int count = 0;
 		for (int i = 1; i <= n; i++) {
 			a[i] = scanner.nextInt();
-			b[i] = a[i];
+			if (a[i] == a[i-1] && a[i] != 3) {
+				a[i] = 0;
+			}else if (a[i] == 3 && a[i-1] != 3) {
+				a[i] = 3 - a[i-1];
+			}
+			if (a[i] == 0) {
+				count++;
+			}
 		}
 		scanner.close();
 		
-		for (int turn = 1; turn <= n; turn++) {
-			for (int i = 1; i <= n; i++) {
-				if (a[i] == 1) {
-					if (a[i+1] == 3) {
-						a[i+1] = 2;
-					}
-					if (a[i-1] == 3) {
-						a[i-1] = 2;
-					}
-				}else if (a[i] == 2) {
-					if (a[i+1] == 3) {
-						a[i+1] = 1;
-					}
-					if (a[i-1] == 3) {
-						a[i-1] = 1;
-					}
-				}
-			}
-		}
-		
-		for (int i = 1; i < n; i++) {
-			if (a[i] == a[i+1]) {
-				if (a[i] != b[i]) {
-					a[i] = 0;
-				}else if (a[i+1] != b[i+1]) {
-					a[i+1] = 0;
-				}else {
-					a[i+1] = 0;
-				}
-			}
-		}
-		
-		int count = 0;
-		for (int i = 1; i <= n; i++) {
-			count += (a[i] == 0)? 1 : 0;
-		}
-		
 		System.out.println(count);
-		for (int i = 1; i <= n; i++) {
-			System.out.print(a[i] + " ");		}
 	}
 }
