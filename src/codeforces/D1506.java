b7c1509f5703c22b2f8f986123f84c7bc59603fa
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2022-09-05 16:52:15 +0700

    Doing 1 exe in codeforces - branch master

---
+++ src/codeforces/D1506.java
@@ -0,0 +1,16 @@
+package codeforces;
+
+import java.util.Scanner;
+
+/**
+ * <a href = "https://codeforces.com/problemset/problem/1506/D"> Link </a>
+ * @author Bris
+ * @since 4:50:45 PM - Sep 5, 2022
+ *
+ */
+public class D1506 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		scanner.close();
+	}
+}
