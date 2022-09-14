f87d86caa441e0a17f42708e4ae3bc88a43b99ab
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2022-09-14 17:34:38 +0700

    +1 exe in codeforces - branch master

--- src/codeforces/D1506.java
+++ src/codeforces/D1506.java
@@ -1,16 +1,34 @@
 package codeforces;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
- * <a href = "https://codeforces.com/problemset/problem/1506/D"> Link </a>
+ * <a href = "https://codeforces.com/contest/1506/problem/D"> Link </a>
  * @author Bris
- * @since 4:50:45 PM - Sep 5, 2022
+ * @since 10:05:33 PM - Sep 13, 2022
  *
  */
 public class D1506 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		while (t-- > 0) {
+			Map<Integer, Integer> map = new LinkedHashMap<>();
+			int maxCount = 0;
+			int n = scanner.nextInt();
+			for (int i = 0; i < n; i++) {
+				int num = scanner.nextInt();
+				if (map.containsKey(num)) {
+					map.put(num, map.get(num) + 1);
+				}else {
+					map.put(num,  1);
+				}
+				maxCount = Math.max(maxCount, map.get(num));
+			}
+			System.out.println((maxCount >= n - maxCount)? 2* maxCount - n : n%2);
+		}
 		scanner.close();
 	}
 }
