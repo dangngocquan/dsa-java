a09248eb9a362c8a1ad922b44cd5c19cf857ee4c
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2022-09-21 19:18:17 +0700

    +1 exe in codeforces - branch master

--- src/codeforces/D1399.java
+++ src/codeforces/D1399.java
@@ -2,43 +2,50 @@ package codeforces;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
- * <a href = "https://codeforces.com/problemset/problem/1399/D"> Link </a>
+ * <a href = "https://codeforces.com/contest/1399/problem/D"> Link </a>
  * @author Bris
- * @since 4:01:50 PM - Sep 17, 2022
+ * @since 7:16:47 PM - Sep 21, 2022
  *
  */
 public class D1399 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int t = scanner.nextInt();
-		StringBuilder str = new StringBuilder();
 		while (t-- > 0) {
 			int n = scanner.nextInt();
-			int[] groupOfChar = new int[n];
 			String s = scanner.next();
-			List<Integer> list = new LinkedList<>();
+			Queue<Integer> q0 = new LinkedList<>();
+			Queue<Integer> q1 = new LinkedList<>();
+			List<Integer> groups = new LinkedList<>();
 			for (int i = 0; i < n; i++) {
-				int value = s.charAt(i) - '0';
-				int index = list.indexOf(1 - value);
-				if (index == -1) {
-					list.add(value);
-					groupOfChar[i] = list.size();
+				if (s.charAt(i) == '0') {
+					if (q1.isEmpty()) {
+						q0.add(q0.size() + 1);
+						groups.add(q0.size());
+					}else {
+						groups.add(q1.peek());
+						q0.add(q1.poll());
+					}
 				}else {
-					list.set(index, value);
-					groupOfChar[i] = index + 1;
+					if (q0.isEmpty()) {
+						q1.add(q1.size() + 1);
+						groups.add(q1.size());
+					}else {
+						groups.add(q0.peek());
+						q1.add(q0.poll());
+					}
 				}
 			}
-			
-			str.append(list.size()).append("\n");
-			for (int i : groupOfChar) {
-				str.append(i).append(" ");
+			System.out.println(q1.size() + q0.size());
+			for (int group: groups) {
+				System.out.print(group + " ");
 			}
-			str.append("\n");
+			System.out.println();
 		}
-		System.out.println(str);
 		scanner.close();
 	}
 }
