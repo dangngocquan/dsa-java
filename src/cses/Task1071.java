084f4a3ff10f8f7f389b6d59c33a47cffe008b05
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-01-20 11:37:31 +0700

    +2 exe in cses

---
+++ src/cses/Task1071.java
@@ -0,0 +1,39 @@
+package cses;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Bris
+ * @since 10:28:23 AM - Jan 20, 2023
+ *
+ */
+public class Task1071 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		StringBuilder str= new StringBuilder();
+		while(t-- > 0) {
+			long y = scanner.nextLong();
+			long x = scanner.nextLong();
+			long maxVal = Math.max(y, x);
+			long minVal = Math.min(y, x);
+			long valBottomRight = maxVal * maxVal - (maxVal - 1);
+			if (maxVal % 2 == 0) {
+				if (y == minVal) {
+					str.append(valBottomRight - (maxVal - minVal) + "\n");
+				}else if (x == minVal) {
+					str.append(valBottomRight + (maxVal - minVal) + "\n");
+				}
+			}else {
+				if (y == minVal) {
+					str.append(valBottomRight + (maxVal - minVal) + "\n");
+				}else if (x == minVal) {
+					str.append(valBottomRight - (maxVal - minVal) + "\n");
+				}
+			}
+		}
+		System.out.println(str);
+		scanner.close();
+	}
+}
---
+++ src/cses/Task1072.java
@@ -0,0 +1,48 @@
+package cses;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Bris
+ * @since 10:52:49 AM - Jan 20, 2023
+ *
+ */
+public class Task1072 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		long n = scanner.nextLong();
+		long k = 0;
+		StringBuilder s = new StringBuilder();
+		while (k++ < n) {
+			if (k == 1) {
+				s.append(0 + "\n");
+			}else if (k == 2) {
+				s.append(6 + "\n");
+			}else if (k == 3) {
+				s.append(28 + "\n");
+			}else if (k == 4) {
+				s.append(96 + "\n");
+			}else if (k == 5) {
+				s.append(252 + "\n");
+			}else if (k == 6) {
+				s.append(550 + "\n");
+			}else {
+				long counter = 0;
+				// 4 corners of k * k
+				counter += 4 * (k*k-2-1);
+				// 4 sides except 4 corner of k * k
+				counter += 4 * (2*(k*k-3-1) + (k-4)*(k*k-4-1));
+				// 4 corners of (k-2) * (k-2)
+				counter += 4 * (k*k-4-1);
+				// 4 sides except 4 corner of (k-2) * (k-2)
+				counter += 4 *(k-4)*(k*k-6-1);
+				// Other
+				counter += (k-4)*(k-4)*(k*k-8-1);
+				s.append(counter/2 + "\n");
+			}
+		}
+		System.out.println(s);
+		scanner.close();
+	}
+}
