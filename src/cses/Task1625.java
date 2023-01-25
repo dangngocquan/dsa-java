eba7f1b24f4f07eed28f42e1d3b77365fa81feba
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-01-25 22:31:12 +0700

    +1 exe in cses

--- src/cses/Task1625.java
+++ src/cses/Task1625.java
@@ -13,255 +13,109 @@ public class Task1625 {
 	public static boolean[][] isUsed;
 	public static int counter = 0;
 	
-	public static boolean checkDead(int indexRow, int indexColumn) {
-		// 0 (1)
-		// 1  0
-		if (indexRow < 6 && indexColumn > 0) {
-			if (!isUsed[indexRow][indexColumn-1] && isUsed[indexRow][indexColumn]
-					&& isUsed[indexRow+1][indexColumn-1] && !isUsed[indexRow+1][indexColumn]) {
-				return true;
-			}
-		}
-		// (1) 0
-		//  0  1
-		if (indexRow < 6 && indexColumn < 6) {
-			if (isUsed[indexRow][indexColumn] && !isUsed[indexRow][indexColumn+1]
-					&& !isUsed[indexRow+1][indexColumn] && isUsed[indexRow+1][indexColumn+1]) {
-				return true;
-			}
-		}
-		//  0  1
-		// (1) 0
-		if (indexRow > 0 && indexColumn < 6) {
-			if (!isUsed[indexRow-1][indexColumn] && isUsed[indexRow-1][indexColumn+1]
-					&& isUsed[indexRow][indexColumn] && !isUsed[indexRow][indexColumn+1]) {
-				return true;
-			}
-		}
-		// 1  0
-		// 0 (1)
-		if (indexRow > 0 && indexColumn > 0) {
-			if (isUsed[indexRow-1][indexColumn-1] && !isUsed[indexRow-1][indexColumn]
-					&& !isUsed[indexRow][indexColumn-1] && isUsed[indexRow][indexColumn]) {
-				return true;
-			}
-		}
-		
-		
-		
-		if (indexRow < 5 && indexColumn > 1) {
-			// 1 1 (1)
-			// 1 0
-			// 1 1
-			if ((isUsed[indexRow][indexColumn-2]) && (isUsed[indexRow][indexColumn-1])
-					&& (isUsed[indexRow+1][indexColumn-2]) && (!isUsed[indexRow+1][indexColumn-1])
-					&& (isUsed[indexRow+2][indexColumn-2]) && (isUsed[indexRow+2][indexColumn-1])
-					) {
-				return true;
-			}
-			//     (1)
-			// 1 0  1
-			// 1 1  1
-			if ((isUsed[indexRow+1][indexColumn-2]) && (!isUsed[indexRow+1][indexColumn-1]) && (isUsed[indexRow+1][indexColumn])
-					&& (isUsed[indexRow+1][indexColumn-2]) && (isUsed[indexRow+1][indexColumn-1]) && (isUsed[indexRow+1][indexColumn])
-					) {
-				return true;
-			}
-		}
-		if (indexRow < 5 && indexColumn < 5) {
-			// (1) 1 1
-			//     0 1
-			//     1 1
-			if ((isUsed[indexRow][indexColumn+1]) && (isUsed[indexRow][indexColumn+2])
-					 && (!isUsed[indexRow+1][indexColumn+1]) && (isUsed[indexRow+1][indexColumn+2])
-					 && (isUsed[indexRow+2][indexColumn+1]) && (isUsed[indexRow+2][indexColumn+2])
-					) {
-				return true;
-			}
-			// (1)
-			//  1  0 1
-			//  1  1 1
-			if ((isUsed[indexRow+1][indexColumn]) && (!isUsed[indexRow+1][indexColumn+1]) && (isUsed[indexRow+1][indexColumn+2])
-					&& (isUsed[indexRow+2][indexColumn]) && (isUsed[indexRow+2][indexColumn+1]) && (isUsed[indexRow+2][indexColumn+2])
-					) {
-				return true;
-			}
-		}
-		if (indexRow > 1 && indexColumn < 5) {
-			//  1  1  1
-			//  1  0  1
-			// (1)
-			if ((isUsed[indexRow-2][indexColumn]) && (isUsed[indexRow-2][indexColumn+1]) && (isUsed[indexRow-2][indexColumn+2])
-					&& (isUsed[indexRow-1][indexColumn]) && (!isUsed[indexRow-1][indexColumn+1]) && (isUsed[indexRow-1][indexColumn+2])
-					) {
-				return true;
-			}
-			//     1  1
-			//     0  1
-			// (1) 1  1
-			if ((isUsed[indexRow-2][indexColumn+1]) && (isUsed[indexRow-2][indexColumn+2])
-					&& (!isUsed[indexRow-1][indexColumn+1]) && (isUsed[indexRow-1][indexColumn+2])
-					&& (isUsed[indexRow][indexColumn+1]) && (isUsed[indexRow][indexColumn+2])
-					) {
-				return true;
-			}
-		}
-		if (indexRow > 1 && indexColumn > 1) {
-			//  1  1  1
-			//  1  0  1
-			//       (1)
-			if ((isUsed[indexRow-2][indexColumn-2]) && (isUsed[indexRow-2][indexColumn-1]) && (isUsed[indexRow-2][indexColumn])
-					&& (isUsed[indexRow-1][indexColumn-2]) && (!isUsed[indexRow-1][indexColumn-1]) && (isUsed[indexRow-1][indexColumn])
-					) {
-				return true;
-			}
-			//  1  1  
-			//  1  0  
-			//  1  1 (1)
-			if ((isUsed[indexRow-2][indexColumn-2]) && (isUsed[indexRow-2][indexColumn-1]) 
-					&& (isUsed[indexRow-1][indexColumn-2]) && (!isUsed[indexRow-1][indexColumn-1])
-					&& (isUsed[indexRow][indexColumn-2]) && (isUsed[indexRow][indexColumn-1]) 
-					) {
-				return true;
-			}
-		}
-		
-		if (indexRow < 6 && indexColumn > 2) {
-			// 1 0 1 (1)
-			// 1 1 1
-			if (isUsed[indexRow][indexColumn-3] && !isUsed[indexRow][indexColumn-2] && isUsed[indexRow][indexColumn-1]
-					&& isUsed[indexRow+1][indexColumn-3] && isUsed[indexRow+1][indexColumn-2] && isUsed[indexRow+1][indexColumn-1]) {
-				return true;
-			}
-		}
-		if (indexRow < 4 && indexColumn > 0) {
-			//   (1)
-			// 1  1
-			// 1  0
-			// 1  1
-			if (isUsed[indexRow+1][indexColumn-1] && isUsed[indexRow+1][indexColumn]
-					&& isUsed[indexRow+2][indexColumn-1] && !isUsed[indexRow+2][indexColumn] 
-					&& isUsed[indexRow+3][indexColumn-1] && isUsed[indexRow+3][indexColumn]) {
-				return true;
-			}
-		}
-		if (indexRow < 6 && indexColumn < 4) {
-			//(1) 1 0 1 
-			//    1 1 1
-			if (isUsed[indexRow][indexColumn+1] && !isUsed[indexRow][indexColumn+2] && isUsed[indexRow][indexColumn+3]
-					&& isUsed[indexRow+1][indexColumn+1] && isUsed[indexRow+1][indexColumn+2] && isUsed[indexRow+1][indexColumn+3]) {
-				return true;
-			}
-		}
-		if (indexRow < 4 && indexColumn < 6) {
-			//(1)
-			// 1  1
-			// 0  1
-			// 1  1
-			if (isUsed[indexRow+1][indexColumn] && isUsed[indexRow+1][indexColumn+1]
-					&& !isUsed[indexRow+2][indexColumn] && isUsed[indexRow+2][indexColumn+1] 
-					&& isUsed[indexRow+3][indexColumn] && isUsed[indexRow+3][indexColumn+1]) {
-				return true;
-			}
-		}
-		if (indexRow > 0 && indexColumn < 4) {
-			// 	   1 1 1 
-			// (1) 1 0 1
-			if (isUsed[indexRow-1][indexColumn+1] && isUsed[indexRow-1][indexColumn+2] && isUsed[indexRow-1][indexColumn+3]
-					&& isUsed[indexRow][indexColumn+1] && !isUsed[indexRow][indexColumn+2] && isUsed[indexRow][indexColumn+3]) {
-				return true;
-			}
-		}
-		if (indexRow > 2 && indexColumn < 6) {
-			// 1  1
-			// 0  1
-			// 1  1
-			//(1)
-			if (isUsed[indexRow-3][indexColumn] && isUsed[indexRow-3][indexColumn+1]
-					&& !isUsed[indexRow-2][indexColumn] && isUsed[indexRow-2][indexColumn+1] 
-					&& isUsed[indexRow-1][indexColumn] && isUsed[indexRow-1][indexColumn+1]) {
-				return true;
-			}
-		}
-		if (indexRow > 0 && indexColumn > 2) {
-			// 	1 1 1 
-			//  1 0 1 (1)
-			if (isUsed[indexRow-1][indexColumn-3] && isUsed[indexRow-1][indexColumn-2] && isUsed[indexRow-1][indexColumn-1]
-					&& isUsed[indexRow][indexColumn-3] && !isUsed[indexRow][indexColumn-2] && isUsed[indexRow][indexColumn-1]) {
-				return true;
-			}
-		}
-		if (indexRow > 2 && indexColumn > 0) {
-			// 1  1
-			// 1  0
-			// 1  1
-			//	 (1)
-			if (isUsed[indexRow-3][indexColumn-1] && isUsed[indexRow-3][indexColumn]
-					&& isUsed[indexRow-2][indexColumn-1] && !isUsed[indexRow-2][indexColumn] 
-					&& isUsed[indexRow-1][indexColumn-1] && isUsed[indexRow-1][indexColumn]) {
-				return true;
-			}
-		}
-		
-		return false;	
-	}
+	
 	
 	public static void solve(int indexChar, int indexRow, int indexColumn) {
-		if (indexChar == 48 && indexRow == 6 && indexColumn == 0) {
-			counter++;
+		if (indexRow == 8 && indexColumn == 2) {
+			if (indexChar == 48) {
+				counter++;
+			}
 		}else {
-			if (indexRow == 1 && indexColumn == 2) {
-				if (isUsed[1][0] && isUsed[1][1] && isUsed[1][2] && !isUsed[0][1]) {
-					return;
-				}
+			// Find correct way: 'D', 'U', 'L', 'R'
+			char correctWay = '?';
+			// (1)			(1)
+			//  0 1   or   1 0
+			//  1			 1
+			if ((isUsed[indexRow+1][indexColumn+1] || isUsed[indexRow+1][indexColumn-1])
+					&& isUsed[indexRow+2][indexColumn] && !isUsed[indexRow+1][indexColumn]) {
+				correctWay = 'D';
 			}
-			if (indexRow == 2 && indexColumn == 1) {
-				if (isUsed[0][1] && isUsed[1][1] && isUsed[2][1] && !isUsed[1][0]) {
-					return;
-				}
+			//  1 			 1 
+			//  0 1   or   1 0
+			// (1)			(1)
+			else if ((isUsed[indexRow-1][indexColumn+1] || isUsed[indexRow-1][indexColumn-1])
+					&& !isUsed[indexRow-1][indexColumn] && isUsed[indexRow-2][indexColumn]) {
+				correctWay = 'U';
 			}
-			if (checkDead(indexRow, indexColumn)) {
-				return;
+			// 1 0 (1)   or    1
+			//   1			 1 0 (1)
+			else if ((isUsed[indexRow+1][indexColumn-1] || isUsed[indexRow-1][indexColumn-1])
+					&& !isUsed[indexRow][indexColumn-1] && isUsed[indexRow][indexColumn-2]) {
+				correctWay = 'L';
 			}
-			if (indexRow == 6 && indexColumn < 6 && !isUsed[indexRow][indexColumn+1]) {
-				return;
-			}
-			if (indexRow == 0 && indexColumn > 0 && !isUsed[indexRow][indexColumn-1]) {
-				return;
-			}
-			if (indexColumn == 6 && indexRow > 0 && !isUsed[indexRow-1][indexColumn]) {
-				return;
-			}
-			if (indexColumn == 0 && indexRow > 0 && !isUsed[indexRow-1][indexColumn]) {
-				return;
+			//(1) 0 1    or      1
+			//    1		     (1) 0 1
+			else if ((isUsed[indexRow+1][indexColumn+1] || isUsed[indexRow-1][indexColumn+1])
+					&& !isUsed[indexRow][indexColumn+1] && isUsed[indexRow][indexColumn+2]) {
+				correctWay = 'R';
 			}
 			
-			if (defaultPath[indexChar] == 'D' || defaultPath[indexChar] == '?') {
-				if (indexRow < 6 && !isUsed[indexRow+1][indexColumn]) {
-					isUsed[indexRow+1][indexColumn] = true;
-					solve(indexChar+1, indexRow+1, indexColumn);
-					isUsed[indexRow+1][indexColumn] = false;
+			// Check input path (default path)
+			char c = defaultPath[indexChar];
+			if (c == '?') {
+				if (correctWay == '?') {
+					// 'D'
+					if (!isUsed[indexRow+1][indexColumn]) {
+						isUsed[indexRow+1][indexColumn] = true;
+						solve(indexChar+1, indexRow+1, indexColumn);
+						isUsed[indexRow+1][indexColumn] = false;
+					}
+					// 'U'
+					if (!isUsed[indexRow-1][indexColumn]) {
+						isUsed[indexRow-1][indexColumn] = true;
+						solve(indexChar+1, indexRow-1, indexColumn);
+						isUsed[indexRow-1][indexColumn] = false;
+					}
+					// 'L'
+					if (!isUsed[indexRow][indexColumn-1]) {
+						isUsed[indexRow][indexColumn-1] = true;
+						solve(indexChar+1, indexRow, indexColumn-1);
+						isUsed[indexRow][indexColumn-1] = false;
+					}
+					// 'R'
+					if (!isUsed[indexRow][indexColumn+1]) {
+						isUsed[indexRow][indexColumn+1] = true;
+						solve(indexChar+1, indexRow, indexColumn+1);
+						isUsed[indexRow][indexColumn+1] = false;
+					}
+				}else {
+					if (correctWay == 'D') {
+						isUsed[indexRow+1][indexColumn] = true;
+						solve(indexChar+1, indexRow+1, indexColumn);
+						isUsed[indexRow+1][indexColumn] = false;
+					}else if (correctWay == 'U') {
+						isUsed[indexRow-1][indexColumn] = true;
+						solve(indexChar+1, indexRow-1, indexColumn);
+						isUsed[indexRow-1][indexColumn] = false;
+					}else if (correctWay == 'L') {
+						isUsed[indexRow][indexColumn-1] = true;
+						solve(indexChar+1, indexRow, indexColumn-1);
+						isUsed[indexRow][indexColumn-1] = false;
+					}else if (correctWay == 'R') {
+						isUsed[indexRow][indexColumn+1] = true;
+						solve(indexChar+1, indexRow, indexColumn+1);
+						isUsed[indexRow][indexColumn+1] = false;
+					}
 				}
-			}
-			if (defaultPath[indexChar] == 'U' || defaultPath[indexChar] == '?') {
-				if (indexRow > 0 && !isUsed[indexRow-1][indexColumn]) {
-					isUsed[indexRow-1][indexColumn] = true;
-					solve(indexChar+1, indexRow-1, indexColumn);
-					isUsed[indexRow-1][indexColumn] = false;
-				}
-			}
-			if (defaultPath[indexChar] == 'L' || defaultPath[indexChar] == '?') {
-				if (indexColumn > 0 && !isUsed[indexRow][indexColumn-1]) {
-					isUsed[indexRow][indexColumn-1] = true;
-					solve(indexChar+1, indexRow, indexColumn-1);
-					isUsed[indexRow][indexColumn-1] = false;
-				}
-			}
-			if (defaultPath[indexChar] == 'R' || defaultPath[indexChar] == '?') {
-				if (indexColumn < 6 && !isUsed[indexRow][indexColumn+1]) {
-					isUsed[indexRow][indexColumn+1] = true;
-					solve(indexChar+1, indexRow, indexColumn+1);
-					isUsed[indexRow][indexColumn+1] = false;
+			}else {
+				if (c == correctWay || correctWay == '?') {
+					if (c == 'D' && !isUsed[indexRow+1][indexColumn]) {
+						isUsed[indexRow+1][indexColumn] = true;
+						solve(indexChar+1, indexRow+1, indexColumn);
+						isUsed[indexRow+1][indexColumn] = false;
+					}else if (c == 'U' && !isUsed[indexRow-1][indexColumn]) {
+						isUsed[indexRow-1][indexColumn] = true;
+						solve(indexChar+1, indexRow-1, indexColumn);
+						isUsed[indexRow-1][indexColumn] = false;
+					}else if (c == 'L' && !isUsed[indexRow][indexColumn-1]) {
+						isUsed[indexRow][indexColumn-1] = true;
+						solve(indexChar+1, indexRow, indexColumn-1);
+						isUsed[indexRow][indexColumn-1] = false;
+					}else if (c == 'R' && !isUsed[indexRow][indexColumn+1]) {
+						isUsed[indexRow][indexColumn+1] = true;
+						solve(indexChar+1, indexRow, indexColumn+1);
+						isUsed[indexRow][indexColumn+1] = false;
+					}
 				}
 			}
 		}
@@ -270,9 +124,21 @@ public class Task1625 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		defaultPath = scanner.next().toCharArray();
-		isUsed = new boolean[7][7];
-		isUsed[0][0] = true;
-		solve(0, 0, 0);
+		isUsed = new boolean[11][11];
+		for (int i = 0; i < 11; i++) {
+			isUsed[0][i] = true;
+			isUsed[1][i] = true;
+			isUsed[9][i] = true;
+			isUsed[10][i] = true;
+			isUsed[i][0] = true;
+			isUsed[i][1] = true;
+			isUsed[i][9] = true;
+			isUsed[i][10] = true;
+		}
+		isUsed[2][2] = true;
+		isUsed[8][1] = false;
+		isUsed[9][2] = false;
+		solve(0, 2, 2);
 	
 		System.out.println(counter);
 		scanner.close();
