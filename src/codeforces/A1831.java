9f9707501424e6055ccdc5d5e99fbb07af860c80
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-05-29 10:19:54 +0700

    ...

--- src/codeforces/A1831.java
+++ src/codeforces/A1831.java
@@ -1,10 +1,13 @@
 package codeforces;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class A1831 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        FastReader scanner = new FastReader();
         int t = scanner.nextInt();
         while (t-- > 0) {
             int n = scanner.nextInt();
@@ -12,6 +15,52 @@ public class A1831 {
                 System.out.print((n+1 - scanner.nextInt()) + " ");
             }
             System.out.println();
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
         }
     }
 }
