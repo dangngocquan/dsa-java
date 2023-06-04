9439a589ff75d564dbf61e4c6465dee5924a54c4
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-06-04 23:43:54 +0700

    +1 exe

---
+++ src/codeforces/A1838.java
@@ -0,0 +1,79 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class A1838 {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader fastReader = new FastReader();
+        int t = fastReader.nextInt();
+        while (t-- > 0) {
+            int n = fastReader.nextInt();
+            int[] a = new int[n];
+            List l = new LinkedList();
+            for (int i = 0; i < n; i++) {
+                a[i] = fastReader.nextInt();
+                if (a[i] < 0) {
+                    l.add(a[i]);
+                }
+            }
+            if (l.size() > 0) {
+                System.out.println(l.get(0));
+                continue;
+            }
+            Arrays.sort(a);
+            System.out.println(a[n - 1]);
+        }
+    }
+}
