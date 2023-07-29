7bf2532d9f5bc0ca50fa8952988fb5ef156206bf
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-07-30 00:08:18 +0700

    +1 exe

---
+++ src/codeforces/A1855.java
@@ -0,0 +1,68 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A1855 {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader fastReader = new FastReader();
+        int t = fastReader.nextInt();
+        while (t-- > 0) {
+            int n = fastReader.nextInt();
+            int count = 0;
+            for (int i = 1; i <= n; i++) {
+                int p = fastReader.nextInt();
+                count += (p == i)? 1 : 0;
+            }
+            System.out.println((count+1)/2);
+        }
+    }
+}
