5c21a67f51ea4eb9dbffeaa755405d1829e87b3c
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-08-12 23:36:35 +0700

    +3 exe

---
+++ src/codeforces/B1859.java
@@ -0,0 +1,160 @@
+package codeforces;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class B1859 {
+    static class FastScanner {
+        private final InputStream in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int bufferPointer = 0;
+        private int bytesRead = 0;
+
+        public FastScanner(InputStream inputStream) {
+            this.in = inputStream;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = in.read(buffer, bufferPointer = 0, buffer.length);
+            if (bytesRead == -1) buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public int nextInt() throws IOException {
+            int number = 0;
+            byte c;
+            // Skip non-digit characters
+            do {
+                c = read();
+            } while (c <= ' ');
+            // Read the number
+            boolean isNegative = false;
+            if (c == '-') {
+                isNegative = true;
+                c = read();
+            }
+            while (c >= '0' && c <= '9') {
+                number = number * 10 + (c - '0');
+                c = read();
+            }
+            return isNegative ? -number : number;
+        }
+
+        public long nextLong() throws IOException {
+            long num = 0;
+            byte c = read();
+            boolean negative = false;
+            while (c <= ' ') {
+                c = read();
+            }
+            if (c == '-') {
+                negative = true;
+                c = read();
+            }
+            while (c >= '0' && c <= '9') {
+                num = num * 10 + (c - '0');
+                c = read();
+            }
+            return negative ? -num : num;
+        }
+
+        public String next() throws IOException {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            // Skip leading whitespace characters
+            do {
+                c = read();
+            } while (c <= ' ');
+            // Read the string
+            while (c > ' ') {
+                sb.append((char) c);
+                c = read();
+            }
+            return sb.toString();
+        }
+
+        public double nextDouble() throws IOException {
+            int integerPart = nextInt();
+            if (read() != '.') {
+                return integerPart;
+            }
+            int fractionalPart = 0;
+            int decimalDivisor = 1;
+
+            while (true) {
+                byte c = read();
+                if (c < '0' || c > '9') {
+                    break;
+                }
+                fractionalPart = fractionalPart * 10 + (c - '0');
+                decimalDivisor *= 10;
+            }
+            return integerPart + (double) fractionalPart / decimalDivisor;
+        }
+    }
+
+
+    static class FastOutput {
+        private BufferedWriter writer;
+        public FastOutput(OutputStream outputStream) {
+            this.writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+        }
+
+        public void print(Object obj) throws IOException {
+            writer.write(obj.toString());
+        }
+
+        public void println(Object obj) throws IOException {
+            writer.write(obj.toString());
+            writer.newLine();
+        }
+
+        public void close() throws IOException {
+            writer.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastScanner fastScanner = new FastScanner(System.in);
+        FastOutput fastOutput = new FastOutput(System.out);
+        int t = fastScanner.nextInt();
+        while (t-- > 0) {
+            int n = fastScanner.nextInt();
+            List<long[]> arrays = new ArrayList<>(n);
+            for (int i = 0; i < n; i++) {
+                int m = fastScanner.nextInt();
+                long[] array = new long[m];
+                for (int j = 0; j < m; j++) {
+                    array[j] = fastScanner.nextLong();
+                }
+                Arrays.sort(array);
+                arrays.add(array);
+            }
+
+            long sum1 = 0;
+            long min0 = arrays.get(0)[0];
+            int indexMin0 = 0;
+            long ans = 0;
+            for (int i = 0; i < n; i++) {
+                long[] array = arrays.get(i);
+                sum1 += (long) array[1];
+                if (min0 > array[0]) {
+                    min0 = array[0];
+                    indexMin0 = i;
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                ans = Math.max(min0 + sum1 - arrays.get(i)[1], ans);
+            }
+            fastOutput.println(ans);
+        }
+        fastOutput.close();
+    }
+}
